4efcc4815729c7d5c6dd904ecd5d367c86a58d05
lmk0420 <lmk19960420@gmail.com>
2020-02-08 23:51:39 +0900

    feature/add-slack-webhooks

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 function main() {
   const properties = getProperties();
+  if (!properties) {
+    return;
+  }
   const DATA_SHEET_BEGIN = 1;
   var sheet = getSheets(properties.SHEET_ID).getSheets()[DATA_SHEET_BEGIN];
 
   const ROW_HEADER = 1
-
   var messageInfos: MessageInfo[] = [];
   sheet.getDataRange().getValues().slice(ROW_HEADER).forEach(v => {
     const sheetRow: SheetRow = new SheetRow(v[0], v[1], v[2], v[3], v[4], v.slice(5));
@@ -15,27 +17,30 @@ function main() {
       return;
     }
 
-    const connpass: Connpass = JSON.parse(res.getContentText())
-    const message: string[] = connpass.events.map(event => { return `${event.title}¥n${event.event_url}` });
+    const connpass: Connpass = JSON.parse(res.getContentText('UTF-8'));
+    const messages: string[] = connpass.events.map(event => { return `${event.title}\n${event.event_url}` });
     if (messageInfos.length === 0) {
       sheetRow.slackIds.forEach(slackId => {
-        messageInfos.push(new MessageInfo(slackId, [sheetRow.keyword], message))
-      })
-    }
-
-    if (messageInfos.length > 0) {
+        messageInfos.push(new MessageInfo(slackId, [sheetRow.keyword], messages));
+      });
+    } else {
       sheetRow.slackIds.forEach(slackId => {
         messageInfos.forEach((messageInfo, i) => {
           if (messageInfo.slackId === slackId) {
-            messageInfos[i].keywords.push(sheetRow.keyword)
-            messageInfos[i].message.push(...message)
+            if (messageInfos[i].keywords.indexOf(sheetRow.keyword) === -1) {
+              messageInfos[i].keywords.push(sheetRow.keyword);
+            }
+            messageInfos[i].message.push(...messages.filter(message => messageInfos[i].message.indexOf(message) === -1))
           } else {
-            messageInfos.push(new MessageInfo(slackId, [sheetRow.keyword], message))
+            messageInfos.push(new MessageInfo(slackId, [sheetRow.keyword], messages));
           }
         });
       });
     }
   });
 
-  Logger.log(messageInfos)
+  messageInfos.forEach(messageInfo => {
+    const message = `検索ワード：*${messageInfo.keywords.join(', ')}*\n\n${messageInfo.message.join(`\n`)}`;
+    callSlackWebhook(messageInfo.slackId, properties.SLACK_WEBHOOK_URL, message);
+  });
 }
--- src/slack.ts
+++ src/slack.ts
@@ -2,3 +2,17 @@ class MessageInfo {
     constructor(public slackId: string, public keywords: string[], public message: string[]) {
     }
 }
+
+function callSlackWebhook(slackId: string, slackWebhookUrl: string, message: string) { 
+  const payload = {
+      channel: `@${slackId}`,
+      text: message,
+      link_names: 1,
+  }
+  const res = UrlFetchApp.fetch(slackWebhookUrl, {
+    method: 'post',
+    payload: JSON.stringify(payload)
+  });
+
+  return res.getContentText('UTF-8')
+}
