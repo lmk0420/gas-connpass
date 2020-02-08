ec30410e011f247aa847dd5cca6cdca2c0faba47
lmk0420 <lmk19960420@gmail.com>
2020-02-08 22:20:56 +0900

    add null check

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,14 @@ function getProperties() {
     const properties = PropertiesService.getScriptProperties()
     const SLACK_ACCESS_TOKEN = properties.getProperty('SLACK_ACCESS_TOKEN')
     const SHEET_ID = properties.getProperty('SHEET_ID')
-    return {
-        SLACK_ACCESS_TOKEN: SLACK_ACCESS_TOKEN,
-        SHEET_ID: SHEET_ID
+    const SLACK_WEBHOOK_URL = properties.getProperty('SLACK_WEBHOOK_URL')
+    
+    if (SLACK_ACCESS_TOKEN && SHEET_ID && SLACK_WEBHOOK_URL) {
+        return {
+            SLACK_ACCESS_TOKEN: SLACK_ACCESS_TOKEN,
+            SHEET_ID: SHEET_ID,
+            SLACK_WEBHOOK_URL: SLACK_WEBHOOK_URL
+        }
     }
+    return null;
   }
