b487d0afa6ec846b595f15968d17a4b1a935564b
lmk0420 <lmk19960420@gmail.com>
2020-06-07 12:12:38 +0900

    blocksに対応

--- src/main.ts
+++ src/main.ts
@@ -26,11 +26,11 @@ function main() {
       if (res.getResponseCode() !== 200) return;
 
       const connpass: Connpass = JSON.parse(res.getContentText("UTF-8"));
-      const messages: string[] = connpass.events.map(event => `${event.title}\n${event.event_url}`);
+      const events: Event[] = connpass.events
       if (messageInfos.length === 0) {
         sheetRow.slackIds.forEach(slackId => {
           messageInfos.push(
-            new MessageInfo(slackId, [sheetRow.keyword], messages)
+            new MessageInfo(slackId, [sheetRow.keyword], events)
           );
         });
       } else {
@@ -40,14 +40,14 @@ function main() {
               if (messageInfos[i].keywords.indexOf(sheetRow.keyword) === -1) {
                 messageInfos[i].keywords.push(sheetRow.keyword);
               }
-              messageInfos[i].message.push(
-                ...messages.filter(
-                  message => messageInfos[i].message.indexOf(message) === -1
+              messageInfos[i].events.push(
+                ...events.filter(
+                  event => messageInfos[i].events.indexOf(event) === -1
                 )
               );
             } else {
               messageInfos.push(
-                new MessageInfo(slackId, [sheetRow.keyword], messages)
+                new MessageInfo(slackId, [sheetRow.keyword], events)
               );
             }
           });
@@ -56,13 +56,14 @@ function main() {
     });
 
   messageInfos.forEach(messageInfo => {
-    const message = `検索ワード：*${messageInfo.keywords.join(
-      ", "
-    )}*\n\n${messageInfo.message.join(`\n`)}`;
+    const title = `検索ワード：${messageInfo.keywords.join(", ")}`;
+    const messages = Array.from(new Set(
+      messageInfo.events.map(event => `・ <${event.event_url}|${event.title}>`)
+    ))
     callSlackWebhook(
       messageInfo.slackId,
       properties.SLACK_WEBHOOK_URL,
-      message
+      createBlocks(title, messages)
     );
   });
 }
--- src/slack.ts
+++ src/slack.ts
@@ -5,7 +5,7 @@ class MessageInfo {
   constructor(
     public slackId: string,
     public keywords: string[],
-    public message: string[]
+    public events: Event[]
   ) {}
 }
 
@@ -13,16 +13,19 @@ class MessageInfo {
  * Send request to Slack webhook.
  * @param slackId slack id
  * @param slackWebhookUrl webhook URL
- * @param message message body
+ * @param blocks blocks
  */
 function callSlackWebhook(
   slackId: string,
   slackWebhookUrl: string,
-  message: string
+  blocks: any
 ) {
   const payload = {
     channel: `@${slackId}`,
-    text: message,
+    // This text is not displayed when using blocks.
+    // Used for notification messages instead.
+    text: "本日の勉強会情報です :fox_face:", 
+    blocks: blocks,
     link_names: 1
   };
   const res = UrlFetchApp.fetch(slackWebhookUrl, {
@@ -31,4 +34,41 @@ function callSlackWebhook(
   });
 
   return res.getContentText("UTF-8");
+}
+
+function createBlocks(title: string, messages: string[]) {
+  const blocks = [
+    {
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text: "本日の勉強会情報です :fox_face:"
+      }
+    }, 
+    {
+      type: "divider"
+    },
+    {
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text: title
+      }
+    }
+  ]
+  Logger.log(messages);
+  
+  messages.forEach(message => blocks.push({
+    type: "section",
+    text: {
+      type: "mrkdwn",
+      text: message
+    }
+  }));
+
+  blocks.push({
+    type: "divider"
+  })
+
+  return blocks
 }
